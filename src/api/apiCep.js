105747d56935ccff85022cdda99bc526660a71c3
Cássio Cauê <cassio3620@gmail.com>
2020-12-30 23:01:37 -0300

    data processing

--- src/api/apiCep.js
+++ src/api/apiCep.js
@@ -1,16 +1,24 @@
 const axios = require('axios').default;
-const clientes = require('../model/clientes');
+
 
 const api = async(cep) =>{
     try {
       const resp = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-      const response = resp.data
-      
-      let cliente = clientes.push(response)
-      console.log(cliente, 'askdmklasmdksmadmasçlmd')
-
-
-      return response;
+      const response = resp.data;
+      const reponseObject = {
+          cep: response.cep,
+          logradouro: response.logradouro,
+          complemento: response.complemento,
+          bairro: response.bairro,
+          localidade: response.localidade,
+          uf: response.uf,
+          ibge: response.ibge,
+          gia: response.gia,
+          ddd: response.ddd,
+          siafi: response.siafi
+      }
+      console.log(reponseObject, 'Dados tratados')
+      return reponseObject;
     
     } catch {
         console.error('Cep não encontrado');
